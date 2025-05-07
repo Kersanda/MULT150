93e81cd179a174005bcb51f5d0c946e7c1f52f91
Kersanda <kersanda@GMAIL.com>
2025-05-07 14:39:50 -0700

    Hour 14 and 16

---
+++ kdelacerda_Hour14/Assets/Scripts/ParticleEffect.cs
@@ -0,0 +1,8 @@
+/*
+Changed:
+Color- Blue
+Duration- 10 seconds
+Added 3D start size - adjusted curve time from 1 to 3, changed the curve by adding a key with a dip
+Added Color over lifetime - red & blue
+Changed shape to sphere
+*/
---
+++ kdelacerda_Hour14/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,8 @@
+/*
+Added an AI generated image of a cat and catnip, adjusted transparancy
+Title - Paws for Nip
+Picked another font option, centered and italic, with different colors
+Description of the game - changed font, color and italicized. 
+Button shaped, color, and color transition change
+
+*/
