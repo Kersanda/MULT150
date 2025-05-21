cbb6140c90424f900d3f272c2f50e837eb613b27
Kersanda <kersanda@GMAIL.com>
2025-05-20 18:37:14 -0700

    Hour_20

---
+++ kdelacerda_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,32 @@
+
+/*  Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+       The game concept is very simple, and I'd say the game itself is very easy. The objective is very simple - see how long you can survive. 
+       The only obstacle - or enemy - is the cube blocks detracting time. The players actions are also very simple - left or right. 
+  What 2 changes would you do to make it easier or harder?
+       Could add the ability to jump, with incoming obstacles that require the player to jump in order to avoid contact.
+       Could also change either the shape or color of some of the objects - maybe only a yellow sphere gives time, but a red sphere detracts.
+       To make it easier, could adjust the time each sphere adds, making the game last longer (not necessarily making it easier, but makes 
+       the playable time longer, or easier to get a higher game time score.)
+  List 2 things that would give it a "wow" factor.
+       Adding a score board that keeps track of the last x amount of runs. 
+       Add the ability for two players to compete against each other, and add different powerups or power downs - think mario kart. For example - 
+       One player could activate a power up that send a massive rolling rock down the other players screen, which takes up half the game area
+       and destroys a few seconds of objects in its path. 
+  What 2 parts of the game are fun? 
+       The game itself is fun in a simple concept way - something you can just semi mindlessly play. Creating the illusion of forward movement
+       was fun, and applying some of the codes we've semi learned into a new way - time adjustment - was fun.  
+  What 2 parts of the game are tedious?
+       The game isn't tedious - if I had taken the time to type out the code instead of copy/paste - that would have been tedious. I do understand
+       that typing it out could/would give me a better idea of what that script is doing, but since I don't have any coding experience, and
+       something simple like absently typing Destroy(other.GameObject); breaks the code, because I decided to capitalize the g in game leaving me 
+       stumped for a few minutes wondering why it isn't working when I ~thought~ I typed it out verbatum. 
+       Perhaps the simple design of the game could be considered tedious, as it doesn't really have an indepth challenge, such as a variety of 
+       interactable objects with different effects.  
+
+Changes
+       Changed the powerup item (PowerUpTime) to a potion bottle, added a light to the potion, modified the particle effects.
+       Added a rotation to the potion bottle. Could not get it to rotate correctly on the Y axis - it seemed to impact its spawn rate and it had a 
+       wide spin radius. it worked fine on the Z axis. 
+       Changed the obstacle (ObstacleBarrier), added a light and particle effect. Added an animation to change the colors. 
+       Changed the color of the walls, so they didn't detract from the obstacles. 
+*/
